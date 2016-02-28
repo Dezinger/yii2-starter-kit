d75974b4b67c54e02e6d2cdd4cd9e419a22e2b74
Aleksey Kazakov <dezinger@gmail.com>
2016-02-28 20:15:06 +0300

    Migration update

--- common/migrations/db/m150725_192740_seed_data.php
+++ common/migrations/db/m150725_192740_seed_data.php
@@ -64,10 +64,51 @@ class m150725_192740_seed_data extends Migration
             'created_at' => time(),
             'updated_at' => time(),
         ]);
+
+        $this->insert('{{%key_storage_item}}', [
+            'key' => 'backend.theme-skin',
+            'value' => 'skin-blue',
+            'comment' => 'skin-blue, skin-black, skin-purple, skin-green, skin-red, skin-yellow'
+        ]);
+
+        $this->insert('{{%key_storage_item}}', [
+            'key' => 'backend.layout-fixed',
+            'value' => 0
+        ]);
+
+        $this->insert('{{%key_storage_item}}', [
+            'key' => 'backend.layout-boxed',
+            'value' => 0
+        ]);
+
+        $this->insert('{{%key_storage_item}}', [
+            'key' => 'backend.layout-collapsed-sidebar',
+            'value' => 0
+        ]);
+
+        $this->insert('{{%key_storage_item}}', [
+            'key' => 'frontend.maintenance',
+            'value' => 'disabled',
+            'comment' => 'Set it to "true" to turn on maintenance mode'
+        ]);
+
     }
 
     public function safeDown()
     {
+        $this->delete('{{%key_storage_item}}', [
+            'key' => 'frontend.maintenance'
+        ]);
+
+        $this->delete('{{%key_storage_item}}', [
+            'key' => [
+                'backend.theme-skin',
+                'backend.layout-fixed',
+                'backend.layout-boxed',
+                'backend.layout-collapsed-sidebar',
+            ],
+        ]);
+
         $this->delete('{{%page}}', [
             'slug' => 'about'
         ]);
