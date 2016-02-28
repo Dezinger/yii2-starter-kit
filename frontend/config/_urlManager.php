555a31c0cde984763a54417367dd392ab72c4fe6
Aleksey Kazakov <dezinger@gmail.com>
2016-02-28 22:39:21 +0300

    Remove API Frontend module

--- frontend/config/_urlManager.php
+++ frontend/config/_urlManager.php
@@ -6,8 +6,5 @@ return [
     'rules'=> [
         // Pages
         ['pattern'=>'page/<slug>', 'route'=>'page/view'],
-
-        // Api
-        ['class' => 'yii\rest\UrlRule', 'controller' => 'api/v1/user', 'only' => ['index', 'view', 'options']]
     ]
 ];
